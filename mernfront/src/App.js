1a630ebcfa068b858a09294ec8bbffe7a488488f
Logan Leopold <loganleopld@me.com>
2019-02-11 17:28:50 -0500

    front end rendering proper

--- mernfront/src/App.js
+++ mernfront/src/App.js
@@ -7,9 +7,8 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Route 
-
-        />
+        <Route path='/notes' render={(routerProps) => 
+          <Note {...routerProps}{...this.state} />}/>
       </div>
     );
   }
