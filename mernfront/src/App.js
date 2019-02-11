5429f67bfae00568a8b9dec2a2a187d0da17f7d3
Logan Leopold <loganleopld@me.com>
2019-02-11 17:02:56 -0500

    stopping for the day

--- mernfront/src/App.js
+++ mernfront/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Route} from 'react-router-dom'
+import Note from './Note'
 import './App.css';
 
 class App extends Component {
@@ -7,7 +8,7 @@ class App extends Component {
     return (
       <div>
         <Route 
-        
+
         />
       </div>
     );
